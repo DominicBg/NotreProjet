338cfbaf91638579d258810e017ddf1d8b53dd8f
benzobubu <maxime.zobenbueler@gmail.com>
2018-11-11 13:40:03 -0500

    Zone de Respawn

---
+++ Assets/RespawnZone.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnZone : Activable {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+
+    public override void Activate(Character[] charToRespawn)
+    {
+
+
+        for(int i = 0; i < charToRespawn.Length; i ++)
+        {
+            if (charToRespawn[i] != null)
+            {
+                charToRespawn[i].Respawn();
+            }
+        }
+         
+
+    }
+
+    public override void Activate()
+    {
+        Debug.Log("ACTIVATE SANS CHAR from : " + this);
+    }
+}
